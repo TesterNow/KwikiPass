5d888fc7b589aef2aeb693883d879313ca9075e4
TesterNow <118944491+TesterNow@users.noreply.github.com>
2023-06-17 19:48:35 -0400

    Update rsa_key_generation.js

--- rsa_key_generation.js
+++ rsa_key_generation.js
@@ -1,1 +1,17 @@
+const crypto = require("crypto");
 
+const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+  modulusLength: 2048,
+});
+
+console.log(
+	publicKey.export({
+		type: "pkcs1",
+		format: "pem",
+	}),
+
+	privateKey.export({
+		type: "pkcs1",
+		format: "pem",
+	})
+)
